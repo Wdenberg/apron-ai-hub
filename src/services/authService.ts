ad61a94dbc969314270df8ac6bfba2d8d4c1a9d4
Wdenberg <Wdenbergskillshd@hotmail.com>
2026-08-07 17:16:33 -0300

    Fix: login com o goole dando erro 404

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -51,3 +51,13 @@ export function onAuthStateChange(
     cb(event);
   });
 }
+export async function signInWithGoogle() {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo: window.location.origin,
+    },
+  });
+
+  if (error) throw error;
+}
